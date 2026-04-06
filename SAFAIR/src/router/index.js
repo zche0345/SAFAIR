d9cef63d86202b18a92c7f8f8faabf9b72aa98d5
zche0345 <zche0345@student.monash.edu>
2026-04-06 14:27:53 +1000

    setup basic structure, added router

---
+++ SAFAIR/src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Homepage from '../pages/homepage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Homepage,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
